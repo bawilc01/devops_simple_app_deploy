8019a52887f08ea30280e336f1e3126c5bb6803e
Brittney Coble <bwilcutt#gmail.com>
2023-07-23 23:26:22 -0400

    added rest funtions for crud api

--- main.go
+++ main.go
@@ -16,6 +16,26 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
+}
+
+type Article struct {
+    Title string `json:"Title"`
+    Desc string `json:"desc"`
+    Content string `json:"content"`
+}
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
+
 func main() {
+	Articles = []Article{
+        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
 	handleRequests()
 }
