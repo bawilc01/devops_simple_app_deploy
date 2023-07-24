7774d44d72482e0c8890604e58f65956a883393f
Brittney Coble <bwilcutt#gmail.com>
2023-07-23 23:28:31 -0400

    adding new route to map calls to function

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	// add our articles route and map it to our 
+    // returnAllArticles function like so
+    http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
